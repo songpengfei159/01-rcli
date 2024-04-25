736b16d3d4ba91ca056f83328fc1b1f446c61d35
zyn <daydayupyo@gmail.com>
2024-04-25 11:28:35 +0800

    first commit

---
+++ src/cli/opts.rs
@@ -0,0 +1,27 @@
+// File: opts.rs
+use clap::Parser;
+
+#[derive(Debug, Parser)]
+pub enum Subcommand {
+    #[command(name = "csv", about = "csv file to json file conversion.")]
+    Csv(CsvOpts),
+}
+//
+#[derive(Debug, Parser)]
+#[clap(name = "rcil", version, author, about,long_about=None)]
+pub struct Opts {
+    #[command(subcommand)]
+    cmd: Subcommand,
+}
+
+#[derive(Debug,Parser)]
+pub struct CsvOpts {
+    #[arg(short, long)]
+    pub input: String,
+    #[arg(short, long, default_value = "output.json")]
+    pub output: String,
+    #[arg(short, long, default_value_t = ',')]
+    pub delimiter: char,
+    #[arg(long, default_value_t = true)]
+    pub header: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+mod cli;
+
+pub use clap::Parser;
+use cli::*;
 fn main() {
-    println!("Hello, world!");
+    // usage: rcil -i <input_file>.csv -o <output_file>.json --delimiter <delimiter>
+    let opts = Opts::parse();
+    println!("{:?}", opts);
 }
